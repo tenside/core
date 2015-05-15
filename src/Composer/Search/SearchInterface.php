c84c4f463f99ad733e037beed2dfb0aba3b471be
Nico Schneider <nico.tcap@gmail.com>
2015-05-15 13:46:36 +0200

    Search interface and an abstract searcher

--- src/Composer/Search/SearchInterface.php
+++ src/Composer/Search/SearchInterface.php
@@ -8,22 +8,79 @@
 
 namespace Tenside\Composer\Search;
 
+use Composer\Package\PackageInterface;
+
 /**
  * Interface SearchInterface
  *
  * @package Tenside\Composer
  */
-interface SearchInterface {
+interface SearchInterface
+{
+
+    /**
+     * The satisfaction threshold indicates the minimum number
+     * of search results needed to bail from searching through
+     * repositories early. This threshold does NOT apply to
+     * the searchFully() method.
+     *
+     * @return int
+     */
+    public function getSatisfactionThreshold();
+
+    /**
+     * Method to set a new satisfaction threshold
+     * on the searcher.
+     *
+     * @param int $satisfactionThreshold
+     *
+     * @return $this
+     */
+    public function setSatisfactionThreshold($satisfactionThreshold);
 
     /**
      * The concrete searcher's search method takes a string
      * argument containing the search keywords, and returns
-     * an array of results.
+     * an array of fully-qualified package names.
      *
-     * @param $keywords
+     * This method MUST respect the satisfaction threshold,
+     * and return a maximum number of results as returned
+     * by the getSatisfactionThreshold() method.
      *
-     * @return array
+     * @param string $keywords
+     *
+     * @return \string[]
      */
     public function search($keywords);
+
+    /**
+     * The concrete searcher's search method takes a string
+     * argument containing the search keywords, and returns
+     * an array of PackageInterface objects.
+     *
+     * This method MUST respect the satisfaction threshold,
+     * and return a maximum number of results as returned
+     * by the getSatisfactionThreshold() method.
+     *
+     * @param string $keywords
+     *
+     * @return PackageInterface[]
+     */
+    public function searchAndDecorate($keywords);
+
+    /**
+     * The concrete searcher's search method takes a string
+     * argument containing the search keywords, and returns
+     * an array of fully-qualified package names.
+     *
+     * This method MUST NOT respect the satisfaction threshold,
+     * and return all search results regardless of the
+     * satisfacetion threshold.
+     *
+     * @param string $keywords
+     *
+     * @return \string[]
+     */
+    public function searchFully($keywords);
 
 }
