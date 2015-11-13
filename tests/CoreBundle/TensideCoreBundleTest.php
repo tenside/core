419677b53b26aec8d558d538e650472e97db0aa4
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-11-13 05:58:13 +0100

    Add test for TensideCoreBundle

---
+++ tests/CoreBundle/TensideCoreBundleTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of tenside/core.
+ *
+ * (c) Christian Schiffler <c.schiffler@cyberspectrum.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    tenside/core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2015 Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @license    https://github.com/tenside/core/blob/master/LICENSE MIT
+ * @link       https://github.com/tenside/core
+ * @filesource
+ */
+
+namespace Tenside\Test\CoreBundle;
+
+use Tenside\CoreBundle\TensideCoreBundle;
+use Tenside\Test\TestCase;
+
+/**
+ * Test the bundle.
+ */
+class TensideCoreBundleTest extends TestCase
+{
+    /**
+     * Test that calling getContainerExtension always creates a new extension.
+     *
+     * @return void
+     */
+    public function testGetContainerExtensionCreatesExtension()
+    {
+        $bundle = new TensideCoreBundle();
+
+        $this->assertInstanceOf(
+            'Tenside\CoreBundle\DependencyInjection\TensideCoreExtension',
+            $first = $bundle->getContainerExtension()
+        );
+
+        $this->assertInstanceOf(
+            'Tenside\CoreBundle\DependencyInjection\TensideCoreExtension',
+            $second = $bundle->getContainerExtension()
+        );
+
+        $this->assertNotSame($first, $second);
+    }
+}
