29f9183d7e32c4cd41a614e6e8bd9ef66eb8fa74
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-11-18 22:19:50 +0100

    Add more unit tests to JsonArray

--- tests/Util/JsonArrayTest.php
+++ tests/Util/JsonArrayTest.php
@@ -36,7 +36,7 @@ class JsonArrayTest extends \PHPUnit_Framework_TestCase
     {
         $json = new JsonArray();
 
-        $this->assertEquals(null, $json->get(''));
+        $this->assertEquals([], $json->get('/'));
         $this->assertEquals(null, $json->get('foo'));
         $this->assertFalse($json->has('foo'));
     }
@@ -80,6 +80,151 @@ class JsonArrayTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Provider method for testGet
+     *
+     * @return array
+     */
+    public function getProvider()
+    {
+        return [
+            [
+                [],
+                [],
+                '/'
+            ],
+            [
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                '/'
+            ],
+            [
+                'value1',
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                '/key1'
+            ],
+        ];
+    }
+
+    /**
+     * Test the get method.
+     *
+     * @param mixed  $expected The expected result.
+     * @param array  $array    The array to check.
+     * @param string $path     The path to retrieve.
+     *
+     * @return void
+     *
+     * @dataProvider getProvider
+     */
+    public function testGet($expected, $array, $path)
+    {
+        $json = new JsonArray($array);
+
+        $this->assertEquals($expected, $json->get($path));
+    }
+
+    /**
+     * Test the get method with forceArray flag.
+     *
+     * @return void
+     */
+    public function testGetForceArray()
+    {
+        $json = new JsonArray(['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3']);
+
+        $this->assertEquals(['value1'], $json->get('/key1', true));
+        $this->assertEquals([], $json->get('/does-not-exist', true));
+    }
+
+    /**
+     * Provider method for testGet
+     *
+     * @return array
+     */
+    public function setProvider()
+    {
+        return [
+            [
+                ['key1' => 'value1'],
+                [],
+                'key1',
+                'value1'
+            ],
+            [
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value33'],
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                '/key3',
+                'value33'
+            ],
+            [
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                [],
+                '/',
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+            ],
+            [
+                ['key1' => 'value1', 'key2' => 'value2'],
+                ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'],
+                '/key3',
+                null,
+            ],
+            [
+                ['key1' => 'value1', 'key2' => 'value2', 'foo' => 'test'],
+                ['key1' => 'value1', 'key2' => 'value2'],
+                '//foo',
+                'test',
+            ],
+        ];
+    }
+
+    /**
+     * Test the set method.
+     *
+     * @param mixed  $expected The expected result array.
+     * @param array  $array    The array to work on.
+     * @param string $path     The path to set.
+     * @param mixed  $value    The value to set.
+     *
+     * @return void
+     *
+     * @dataProvider setProvider
+     */
+    public function testSet($expected, $array, $path, $value)
+    {
+        $json = new JsonArray($array);
+
+        $this->assertEquals($json, $json->set($path, $value));
+        $this->assertEquals($expected, $json->getData());
+    }
+
+    /**
+     * Test the remove method.
+     *
+     * @return void
+     */
+    public function testRemove()
+    {
+        $json = new JsonArray(['test' => ['key1' => 'value1', 'key2' => 'value2', 'bar/baz' => 'foo']]);
+
+        $this->assertEquals($json, $json->remove('/test/key2'));
+        $this->assertEquals(['test' => ['key1' => 'value1', 'bar/baz' => 'foo']], $json->getData());
+    }
+
+    /**
+     * Test the isEmpty method.
+     *
+     * @return void
+     */
+    public function testIsEmpty()
+    {
+        $json = new JsonArray(['test' => ['key1' => '', 'key2' => 'false']]);
+
+        $this->assertTrue($json->isEmpty('key1'));
+        $this->assertTrue($json->isEmpty('key2'));
+        $this->assertTrue($json->isEmpty('key3'));
+    }
+
+    /**
      * Test the get entries method.
      *
      * @return void
@@ -90,5 +235,33 @@ class JsonArrayTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(['test'], $json->getEntries('/'));
         $this->assertEquals(['test/key1', 'test/key2', 'test/bar\/baz'], $json->getEntries('/test'));
+    }
+
+    /**
+     * Test the merge method.
+     *
+     * @return void
+     */
+    public function testMerge()
+    {
+        $json = new JsonArray(['test' => ['key1' => 'value1', 'key2' => 'value2']]);
+        $json->merge(['test' => ['key1' => 'value1', 'key2' => 'foobar', 'key3' => 'three']]);
+
+        $this->assertEquals(['test'], $json->getEntries('/'));
+        $this->assertEquals(['test/key1', 'test/key2', 'test/key3'], $json->getEntries('/test'));
+
+        $this->assertEquals('foobar', $json->get('/test/key2'));
+    }
+
+    /**
+     * Test that an exception is thrown for invalid json data.
+     *
+     * @return void
+     *
+     * @expectedException \RuntimeException
+     */
+    public function testBailsWithBrokenJson()
+    {
+        new JsonArray('{"foo": "bar",}');
     }
 }
